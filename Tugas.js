77b395bfe3b8625fd89d80622ff99b209a0ff7f1
abdulmuhyiismail <114898486+abdulmuhyiismail@users.noreply.github.com>
2023-10-03 11:52:00 +0700

    Add files via upload

---
+++ Tugas.js
@@ -0,0 +1,8 @@
+const firstName = "Abdul";
+const lastName = "Muhyi Ismail";
+const age = 19;
+const isMarried = false;
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(isMarried); 
